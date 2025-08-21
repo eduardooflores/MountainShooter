84338c9765e49a5b3fd41c8a9436160cc6a67f92
Eduardo Flores <eduardofloresdev@gmail.com>
2025-08-20 21:49:44 -0300

    Creating menu interactions Creating ABC class Creating Factory Level 1 Bg implementation: Parallax

--- code/Menu.py
+++ code/Menu.py
@@ -27,9 +27,9 @@ class Menu:
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                    self.menu_text(30, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
                 else:
-                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                    self.menu_text(30, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()
 
             # Check for all events
